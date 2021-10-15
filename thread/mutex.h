f682d0ef92d1cc461878ca79424a9486f354c232
Guillaume Militello <guillaume.militello@gmail.com>
2021-10-15 06:46:35 -0400

    Add mutex, shared mutex and lock basic implementation

---
+++ thread/mutex.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+
+class Mutex
+{
+    std::atomic_flag _flag;
+
+public:
+    Mutex(): _flag(ATOMIC_FLAG_INIT) {}
+
+    void lock()
+    {
+        while(_flag.test_and_set(std::memory_order_acquire));
+    }
+
+    void unlock()
+    {
+        _flag.clear(std::memory_order_release);
+    }
+};
+
+class SharedMutex
+{
+     Mutex _mutex;
+     int _readers = 0;
+     bool _writer = false;
+     std::condition_variable_any _unlocked;
+
+public:
+     SharedMutex() = default;
+     SharedMutex(const SharedMutex &) = delete;
+     SharedMutex &operator=(const SharedMutex &) = delete;
+
+     void read_lock()
+     {
+          _mutex.lock();
+          while (_writer)
+          {
+               _unlocked.wait(_mutex);
+          }
+          ++_readers;
+          _mutex.unlock();
+     }
+
+     void read_unlock()
+     {
+          _mutex.lock();
+          --_readers;
+          if (_readers == 0)
+          {
+               _unlocked.notify_all();
+          }
+          _mutex.unlock();
+     }
+
+     void write_lock()
+     {
+          _mutex.lock();
+          while (_writer || _readers > 0)
+          {
+               _unlocked.wait(_mutex);
+          }
+          _writer = true;
+          _mutex.unlock();
+     }
+
+     void write_unlock()
+     {
+          _mutex.lock();
+          _writer = false;
+          _unlocked.notify_all();
+          _mutex.unlock();
+     }
+};
+
+class ScopedReadLock
+{
+     SharedMutex& _mutex;
+
+public:
+     ScopedReadLock(SharedMutex& mutex_) : _mutex(mutex_)
+     {
+          _mutex.read_lock();
+     }
+
+     ~ScopedReadLock()
+     {
+          _mutex.read_unlock();
+     }
+};
+
+class ScopedWriteLock
+{
+     SharedMutex& _mutex;
+
+public:
+     ScopedWriteLock(SharedMutex& mutex_) : _mutex(mutex_)
+     {
+          _mutex.write_lock();
+     }
+
+     ~ScopedWriteLock()
+     {
+          _mutex.write_unlock();
+     }
+};
+
