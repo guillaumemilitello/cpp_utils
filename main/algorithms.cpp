86b9559abaeeabfa999839c26a7293a315a99e1c
Guillaume Militello <guillaume.militello@gmail.com>
2023-07-12 16:43:15 +0000

    Update algorithms with google benchmark

--- main/algorithms.cpp
+++ main/algorithms.cpp
@@ -1,62 +1,92 @@
 #include <iostream>
+#include <functional>
 #include <stream.hpp>
 #include <random.hpp>
 #include <sorting.hpp>
 #include <searching.hpp>
 
-int main(int argc, char *argv[])
+#include "benchmark/benchmark.h"
+
+template <class T>
+using SortFunc = std::function<void(std::vector<T>&)>;
+
+template <class T>
+void BM_Sort(benchmark::State& state, SortFunc<T> F)
 {
-    constexpr double MIN  {0};
-    constexpr double MAX  {30};
-    constexpr double SIZE {25000};
-
+    const auto N {state.range(0)};
+    auto doubleVect {getRandVect<T>(N)};
+    while (state.KeepRunning())
     {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::bubbleSort(doubleVect);
+        F(doubleVect);
     }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::bucketSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::heapSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::insertionSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::mergeSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::quickSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::selectionSort(doubleVect);
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        sorting::selectionSort(doubleVect); // precondition: arr is sorted (in ascending order)
-        const auto indVect {getRandVect<size_t>(0, SIZE, 100)};
-        std::for_each(indVect.begin(), indVect.end(), [&doubleVect](size_t i) { searching::binarySearch(doubleVect, doubleVect[i]); });
-    }
-
-    {
-        auto doubleVect {getRandVect<double>(MIN, MAX, SIZE)};
-        const auto indVect {getRandVect<size_t>(0, SIZE, 100)};
-        std::for_each(indVect.begin(), indVect.end(), [&doubleVect](size_t i) { searching::linearSearch(doubleVect, doubleVect[i]); });
-    }
-
-    return 0;
+    state.SetItemsProcessed(N * state.iterations());
 }
+
+template <class T>
+void BM_bubbleSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::bubbleSort<T>));
+}
+
+template <class T>
+void BM_bucketSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::bucketSort<T>));
+}
+
+template <class T>
+void BM_heapSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::heapSort<T>));
+}
+
+template <class T>
+void BM_insertionSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::insertionSort<T>));
+}
+
+template <class T>
+void BM_mergeSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::mergeSort<T>));
+}
+
+template <class T>
+void BM_quickSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::quickSort<T>));
+}
+
+template <class T>
+void BM_selectionSort(benchmark::State& state)
+{
+    BM_Sort<T>(state, &(sorting::selectionSort<T>));
+}
+
+// {
+//     auto doubleVect {getRandVect<T>(MIN, MAX, SIZE)};
+//     sorting::selectionSort(doubleVect); // precondition: arr is sorted (in ascending order)
+//     const auto indVect {getRandVect<size_t>(0, SIZE, 100)};
+//     std::for_each(indVect.begin(), indVect.end(), [&doubleVect](size_t i) { searching::binarySearch(doubleVect, doubleVect[i]); });
+// }
+
+// {
+//     auto doubleVect {getRandVect<T>(MIN, MAX, SIZE)};
+//     const auto indVect {getRandVect<size_t>(0, SIZE, 100)};
+//     std::for_each(indVect.begin(), indVect.end(), [&doubleVect](size_t i) { searching::linearSearch(doubleVect, doubleVect[i]); });
+// }
+
+#define ARGS \
+    ->MinTime(2) \
+    ->Arg(1<<8)
+
+BENCHMARK_TEMPLATE1(BM_bubbleSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_bucketSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_heapSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_insertionSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_mergeSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_quickSort, double) ARGS;
+BENCHMARK_TEMPLATE1(BM_selectionSort, double) ARGS;
+
+BENCHMARK_MAIN();
