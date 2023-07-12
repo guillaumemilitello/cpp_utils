4793749764134a467b31de1109e15d9ff84b81ed
Guillaume Militello <guillaume.militello@gmail.com>
2023-07-12 16:41:48 +0000

    Update random vector generation

--- lib/utility/random.hpp
+++ lib/utility/random.hpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <vector>
+#include <limits>
 
 template<typename T>
 T getRand(const T& min_, const T& max_)
@@ -18,7 +19,7 @@ T getRand(const T& min_, const T& max_)
 }
 
 template<typename T>
-std::vector<T> getRandVect(const T& min_, const T& max_, size_t size_)
+std::vector<T> getRandVect(size_t size_, const T min_=std::numeric_limits<T>::min(), const T max_=std::numeric_limits<T>::max())
 {
     std::vector<T> res(size_);
     for(size_t i=0; i<size_; ++i)
